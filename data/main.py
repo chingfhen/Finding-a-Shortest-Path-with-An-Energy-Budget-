82c58245a4cd9db276b2652d967e1c9d93d3e8af
timothytancy <timothytan1999@gmail.com>
2021-10-09 21:58:19 +0800

    figures and added task 3 code to main.py
    sorry code very messy my python linter is down and im lazy to write properly

--- data/main.py
+++ data/main.py
@@ -1,31 +1,37 @@
+from task3 import *
+from task2 import *
+from task1 import *
 import json
 import numpy as np
 import time
-import os 
+import os
 
 
 # read data
-with open(r"..\data\G.json", encoding = "utf8") as f:
+with open(r"..\data\G.json", encoding="utf8") as f:
     G = json.load(f)
-with open(r"..\data\Dist.json", encoding = "utf8") as f:
+with open(r"..\data\Dist.json", encoding="utf8") as f:
     Dist = json.load(f)
-with open(r"..\data\Cost.json", encoding = "utf8") as f:
+with open(r"..\data\Cost.json", encoding="utf8") as f:
     Cost = json.load(f)
-with open(r"..\data\Coord.json", encoding = "utf8") as f:
+with open(r"..\data\Coord.json", encoding="utf8") as f:
     Coord = json.load(f)
-    
-# these functions read the distance and energy of an edge from the data respectively 
-def distance_func(u,v):
-    return Dist[f"{u},{v}"]
-def energy_func(u,v):
-    return Cost[f"{u},{v}"]  
 
+# these functions read the distance and energy of an edge from the data respectively
+
+
+def distance_func(u, v):
+    return Dist[f"{u},{v}"]
+
+
+def energy_func(u, v):
+    return Cost[f"{u},{v}"]
 
 
 """
 TASK 1
 """
-from task1 import *
+
 start = "1"
 end = "50"
 print("Task 1 results:")
@@ -38,10 +44,10 @@ print("Time taken for task 1:", end_time - start_time, "\n")
 """
 TASK 2
 """
-from task2 import *
 
 start = time.time()
-shortest = find_path(G, "1", "50", cost_func=distance_func, energy_func = energy_func, energy_budget = 287932)
+shortest = find_path(G, "1", "50", cost_func=distance_func,
+                     energy_func=energy_func, energy_budget=287932)
 end = time.time()
 print("Task 2 results:")
 print("Shortest path: ", "->".join(shortest.nodes))
@@ -49,3 +55,24 @@ print("Shortest distance: ", shortest.distance)
 print("Total energy cost: ", shortest.energy)
 print("Time taken for task 2: ", end - start, "\n")
 
+"""
+TASK 3
+"""
+
+
+def heuristic_sl_distance(alpha, node1, node2="50"):
+    x_dist = abs(Coord[node1][0])-abs(Coord[node2][0])
+    y_dist = abs(Coord[node1][1])-abs(Coord[node2][1])
+    dist = math.sqrt(x_dist**2 + y_dist**2)
+    return alpha*dist
+
+
+start = time.time()
+shortest = find_path_astar(G, "1", "50", cost_func=distance_func, energy_func=energy_func,
+                           heuristic_func=heuristic_sl_distance, alpha=alpha, energy_budget=287932)
+end = time.time()
+print("Task 3 results:")
+print("Shortest path: ", "->".join(shortest.nodes))
+print("Shortest distance: ", shortest.distance)
+print("Total energy cost: ", shortest.energy)
+print("Time taken for task 2: ", end - start, "\n")
--- data/task3.py
+++ data/task3.py
@@ -1,4 +1,3 @@
-# %%
 import math
 import time
 import json
@@ -9,17 +8,17 @@ from heapq import heappush
 
 
 def find_path_astar(
-    graph, s, d, cost_func, energy_func, heuristic_func, energy_budget=287932
+    graph, s, d, cost_func, energy_func, heuristic_func, alpha, energy_budget=287932
 ):
 
     predecessors = astar(
-        graph, s, d, cost_func, energy_func, heuristic_func, energy_budget
+        graph, s, d, cost_func, energy_func, heuristic_func, alpha, energy_budget
     )
 
     return extract_shortest_path_from_predecessor_list(predecessors, d)
 
 
-def astar(graph, s, d, cost_func, energy_func, heuristic_func, energy_budget=287932):
+def astar(graph, s, d, cost_func, energy_func, heuristic_func, alpha, energy_budget=287932):
     # optional - allows the algorithm to rerun multiple times
     graph = graph.copy()
 
@@ -35,7 +34,7 @@ def astar(graph, s, d, cost_func, energy_func, heuristic_func, energy_budget=287
         predecessors = {s: (None, None, None)}
 
         # (f_score of u, cost_of_s_to_u, node)
-        visit_queue = [(heuristic_func(s), 0, s)]
+        visit_queue = [(heuristic_func(alpha, s), 0, s)]
 
         # visited nodes - guaranteed with the lowest cost_of_s_to_u
         visited = set()
@@ -59,7 +58,7 @@ def astar(graph, s, d, cost_func, energy_func, heuristic_func, energy_budget=287
                 continue
 
             # sort neighbours based on heuristic
-            neighbors = sort_neighbors(neighbors, heuristic_func)
+            neighbors = sort_neighbors(neighbors, heuristic_func, alpha)
 
             # Check each of u's neighboring nodes to see if we can update costs
             for v in neighbors:
@@ -73,7 +72,8 @@ def astar(graph, s, d, cost_func, energy_func, heuristic_func, energy_budget=287
 
                 # add "estimated" cost from v to d    ( f = g + h )
 
-                f_score = cost_of_s_to_u_plus_cost_of_e + heuristic_func(v)
+                f_score = cost_of_s_to_u_plus_cost_of_e + \
+                    heuristic_func(alpha, v)
 
                 # If there are no existing costs, UPDATE.
                 # If the new cost found is lower, UPDATE.
@@ -109,57 +109,7 @@ def astar(graph, s, d, cost_func, energy_func, heuristic_func, energy_budget=287
     return predecessors
 
 
-def sort_neighbors(neighbors, heuristic_func):
-    neighbor_dict = {v: heuristic_func(v) for v in neighbors}
+def sort_neighbors(neighbors, heuristic_func, alpha):
+    neighbor_dict = {v: heuristic_func(alpha, v) for v in neighbors}
     sorted_neighbors = sorted(neighbors, key=neighbor_dict.get)
     return sorted_neighbors
-
-
-with open(r"../data/G.json", encoding="utf8") as f:
-    G = json.load(f)
-with open(r"../data/Dist.json", encoding="utf8") as f:
-    Dist = json.load(f)
-with open(r"../data/Cost.json", encoding="utf8") as f:
-    Cost = json.load(f)
-with open(r"../data/Coord.json", encoding="utf8") as f:
-    Coord = json.load(f)
-
-
-def distance_func(u, v):
-    return Dist[f"{u},{v}"]
-
-
-def energy_func(u, v):
-    return Cost[f"{u},{v}"]
-
-
-def heuristic_sl_distance(node1, node2="50", alpha=1):
-    x_dist = abs(Coord[node1][0])-abs(Coord[node2][0])
-    y_dist = abs(Coord[node1][1])-abs(Coord[node2][1])
-    dist = math.sqrt(x_dist**2 + y_dist**2)
-    return alpha * dist
-
-
-# %%
-# A* single run
-start = time.time()
-shortest = find_path_astar(G, "1", "50", cost_func=distance_func, energy_func=energy_func,
-                           heuristic_func=heuristic_sl_distance, energy_budget=287932)
-end = time.time()
-print(shortest)
-print(end-start)
-
-# %%
-# average time for 100 runs heuristic A*
-times = []
-for i in range(100):
-    start = time.time()
-    shortest = find_path_astar(G, "1", "50", cost_func=distance_func, energy_func=energy_func,
-                               heuristic_func=heuristic_sl_distance, energy_budget=287932)
-    end = time.time()
-    times.append(end-start)
-
-avg_time = sum(times)/len(times)
-print(avg_time)
-
-# %%
